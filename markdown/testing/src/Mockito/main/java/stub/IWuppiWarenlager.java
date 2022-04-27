2e62b8cc7012d368e450418ac0e846dbeaaa04dd
jedi101 <mythos007@gmx.de>
2022-04-27 18:27:30 +0200

    Added Stub example test src

---
+++ markdown/testing/src/Mockito/main/java/stub/IWuppiWarenlager.java
@@ -0,0 +1,11 @@
+package stub;
+
+import java.util.List;
+
+/**
+ * Das Warenlager für die Wuppis gibt es noch gar nicht es befindet sich derzeit aber bereits im
+ * Bau. Später sollen darüber alle Wuppis bestellt werden können.
+ */
+public interface IWuppiWarenlager {
+    public List<String> getAlleWuppis();
+}
---
+++ markdown/testing/src/Mockito/test/java/stub/WuppiWarenlagerStubTest.java
@@ -0,0 +1,32 @@
+package stub;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unsere Testklasse die wir nutzen, um über unsere heißen Wuppistores alle Wuppis aus dem zuvor
+ * erzeugten WuppiWarenlagerStub zu bestellen und diese funktionalität testen zu können, obwohl noch
+ * gar kein richtiges Warenlager existiert.
+ */
+public class WuppiWarenlagerStubTest {
+    @Test
+    public void testBestellung() {
+
+        // Wir nutzen kurzerhand einen Stub des eigentlich noch nicht
+        // fertiggestellten Warenlagers, um schonmal die Funktionalität zu
+        // testen.
+        IWuppiWarenlager lager = new IWuppiWarenlagerStub();
+
+        // Unser neuer, brandheißer WuppiStore!
+        WuppiStore wuppiStore = new WuppiStore(lager);
+
+        // Bestelle alle Wuppis aus dem Lagerstub.
+        List<String> bestellteWuppis = wuppiStore.bestelleAlleWuppis(lager);
+
+        // Hat die Bestellung geklappt?
+        assertEquals(2, bestellteWuppis.size());
+    }
+}
