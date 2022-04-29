c54650dd617a76cde3f228451b612f02b69091ea
Carsten Gips <cagix@fh-bielefeld.de>
2022-04-29 13:53:18 +0200

    add Studi-LSF example

---
+++ markdown/testing/src/mockito/src/main/java/fhb/LSF.java
@@ -0,0 +1,2 @@
+package fhb;public class LSF {
+}
---
+++ markdown/testing/src/mockito/src/main/java/fhb/Studi.java
@@ -0,0 +1,31 @@
+package fhb;
+
+public class Studi {
+    String name;
+    LSF lsf;
+
+    public Studi(String name, LSF lsf) {
+        this.name = name;
+        this.lsf = lsf;
+    }
+
+    /**
+     * Melde den Studi zur Prüfung an.
+     *
+     * @param modul In diesem Modul will sich der Studi anmelden
+     * @return true, wenn Anmeldung erfolgreich; sonst false
+     */
+    public boolean anmelden(String modul) {
+        return lsf.anmelden(name, modul);
+    }
+
+    /**
+     * Nehme an der Klausureinsicht teil und reagiere auf das Ergebnis.
+     *
+     * @param modul Für dieses Modul soll die Einsicht erfolgen
+     * @return true, falls mehr als 50 Punkte (bestanden); sonst false
+     */
+    public boolean klausurEinsicht(String modul) {
+        return lsf.ergebnis(name, modul) > 50;
+    }
+}
