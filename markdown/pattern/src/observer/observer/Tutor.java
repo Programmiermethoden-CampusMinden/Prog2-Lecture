66661027dd4e0fc460ecdd39dc7f4af02833deb4
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-07 07:33:02 +0200

    fix typo

--- markdown/pattern/src/observer/observer/Tutor.java
+++ markdown/pattern/src/observer/observer/Tutor.java
@@ -2,7 +2,7 @@ package observer;
 
 import java.util.logging.Logger;
 
-public class Tutor extends Person implements Observer {
+public class Tutor extends Student implements Observer {
     private static final Logger LOGGER = Logger.getLogger(Tutor.class.getName());
 
     public Tutor(String name) {
