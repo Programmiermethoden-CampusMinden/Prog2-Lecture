d537681279dcee4d49e53f6e44bc62454e62e5ca
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-09 16:52:43 +0200

    Vorlesung: Pattern: Singleton (#440)
    * add singleton
    
    * Singleton: Fix Literature
    
    * update schedule: swap serialisation w/ singleton
    
    * update examples
    
    * überarbeite singleton
    
    * improve warning
    
    * improve format
    
    * add videos
    
    * add quiz
    
    * Update markdown/pattern/singleton.md
    
    * Update markdown/pattern/singleton.md

---
+++ markdown/pattern/src/singleton/SingletonEager.java
@@ -0,0 +1,16 @@
+package singleton;
+
+/**
+ * Demonstration einer häufig verwendeten Ausprägung des Singleton-Patterns: "Eager Singleton":
+ * Die Instanz der Klasse wird direkt beim Laden durch JVM angelegt.
+ */
+public class SingletonEager {
+    private static final SingletonEager inst = new SingletonEager();
+
+    // Privater Constructor: Niemand kann Objekte außerhalb der Klasse anlegen
+    private SingletonEager() {}
+
+    public static SingletonEager getInst() {
+        return inst;
+    }
+}
