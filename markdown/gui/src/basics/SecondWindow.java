f7f319a8830f054b8878adc902ee1f8a6cbb1810
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-18 08:01:14 +0200

    add examples

---
+++ markdown/gui/src/basics/SecondWindow.java
@@ -0,0 +1,41 @@
+package basics;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/** Erzeuge ein Fenster mit einem Button, einem Label und einem festen Textfeld */
+public class SecondWindow {
+    /** Erzeuge die Komponenten in neuem EDT-Job statt im main()-Tread */
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(
+                new Runnable() {
+                    public void run() {
+
+                        JFrame frame = new JFrame("SecondWindow");
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                        JButton button = new JButton("Button");
+                        button.setToolTipText("ich bin ein tool-tipp-text");
+
+                        JLabel label = new JLabel("Label");
+
+                        JTextField textField = new JTextField();
+                        textField.setColumns(20);
+                        textField.setEditable(false);
+
+                        JPanel contentPane = new JPanel();
+                        contentPane.add(button);
+                        contentPane.add(label);
+                        contentPane.add(textField);
+
+                        frame.add(contentPane);
+
+                        frame.pack();
+                        frame.setVisible(true);
+                    }
+                });
+    }
+}
