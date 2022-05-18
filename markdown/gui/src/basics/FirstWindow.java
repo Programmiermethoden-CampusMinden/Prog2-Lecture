13d1452bb38e0aff2003ae91535811796cdc5f41
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-18 08:18:41 +0200

    verbessere Code

--- markdown/gui/src/basics/FirstWindow.java
+++ markdown/gui/src/basics/FirstWindow.java
@@ -1,12 +1,13 @@
 package basics;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /** Erzeuge ein minimales Fenster */
 public class FirstWindow {
     /** Erzeuge die Komponenten in neuem EDT-Job statt im main()-Tread */
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(
+        SwingUtilities.invokeLater(
                 new Runnable() {
                     public void run() {
                         JFrame frame = new JFrame("Hello World :)");
--- markdown/gui/src/basics/SecondWindow.java
+++ markdown/gui/src/basics/SecondWindow.java
@@ -5,12 +5,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 /** Erzeuge ein Fenster mit einem Button, einem Label und einem festen Textfeld */
 public class SecondWindow {
     /** Erzeuge die Komponenten in neuem EDT-Job statt im main()-Tread */
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(
+        SwingUtilities.invokeLater(
                 new Runnable() {
                     public void run() {
 
