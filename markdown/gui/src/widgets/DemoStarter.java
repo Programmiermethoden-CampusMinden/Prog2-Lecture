d00de43ec438fd42c29cdc4a6fa1f8ee077cba2d
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-18 20:54:36 +0200

    add final examples

--- markdown/gui/src/widgets/DemoStarter.java
+++ markdown/gui/src/widgets/DemoStarter.java
@@ -13,10 +13,17 @@ public class DemoStarter {
                         JFrame frame = new JFrame("Widget Demo");
                         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                        // frame.setContentPane(new RadioButtonDemo());
+                        frame.setContentPane(new RadioButtonDemo());
                         // frame.setContentPane(new FileChooserDemo());
                         // frame.setContentPane(new TabbedPaneDemo());
-                        frame.setContentPane(new PopupDemo());
+                        // frame.setContentPane(new PopupDemo());
+                        // frame.setContentPane(new DialogDemo());
+
+                        /*
+                        frame.setContentPane(new MenuDemo());
+                        //  Die Menüleiste gehört zum Frame, nicht zum Panel!
+                        frame.setJMenuBar(MenuDemo.newMenuBar());
+                        */
 
                         frame.pack();
                         frame.setVisible(true);
