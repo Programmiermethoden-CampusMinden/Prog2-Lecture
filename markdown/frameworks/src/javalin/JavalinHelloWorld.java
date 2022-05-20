72f8ede8fdf7fe6b1022b046ab65a95ea11b2f11
Andre Matutat <andre.matutat@fh-bielefelde.de>
2022-05-20 14:14:46 +0200

    improve example

--- markdown/frameworks/src/javalin/JavalinHelloWorld.java
+++ markdown/frameworks/src/javalin/JavalinHelloWorld.java
@@ -1,5 +1,3 @@
-package javalin;
-
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -7,17 +5,30 @@ import org.jetbrains.annotations.NotNull;
 
 public class JavalinHelloWorld {
     public static void main(String[] args) {
-        //Starte den Webserver auf Port 8080
+        // Starte den Webserver auf Port 8080
         Javalin app = Javalin.create().start(8080);
 
-        //Registirere eine Handler der aufgerufen wird, wenn eine HTTP-Anfrage auf "localhost:8080/" eingeht.
-        app.get("/", new Handler() {
-            @Override
-            //Context: https://javalin.io/documentation#context
-            public void handle(@NotNull Context ctx) throws Exception {
-                ctx.result("Hello World");
-            }
-        });
+        // Registirere eine Handler der aufgerufen wird, wenn eine HTTP-Anfrage auf
+        // "localhost:8080/" eingeht.
+        app.get(
+                "/",
+                new Handler() {
+                    @Override
+                    // Context: https://javalin.io/documentation#context
+                    public void handle(@NotNull Context ctx) throws Exception {
+                        ctx.result("Hello World");
+                    }
+                });
+
+        // Registirere eine Handler der aufgerufen wird, wenn eine HTTP-Anfrage auf
+        // "localhost:8080/exit" eingeht.
+        app.get(
+                "/exit",
+                new Handler() {
+                    @Override
+                    public void handle(@NotNull Context ctx) throws Exception {
+                        System.exit(0);
+                    }
+                });
     }
 }
-
