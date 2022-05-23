a564e972a841d35fd517b302c6f7428b6c87da31
Carsten Gips <cagix@fh-bielefeld.de>
2022-05-23 18:57:17 +0200

    refactor sources

--- markdown/frameworks/src/javalin/src/main/java/javalin/JavalinHelloWorld.java
+++ markdown/frameworks/src/javalin/src/main/java/javalin/JavalinHelloWorld.java
@@ -1,32 +1,34 @@
+package javalin;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
 
+/** Demo für Javalin: Hello World (MWE) */
 public class JavalinHelloWorld {
+    /** Starte die Demo */
     public static void main(String[] args) {
-        // Starte den Webserver auf Port 8080
+        // Starte den Webserver auf Port 8080: "localhost:8080/"
         Javalin app = Javalin.create().start(8080);
 
-        // Registriere einen Handler, der aufgerufen wird, wenn eine HTTP-Anfrage auf
-        // "localhost:8080/" eingeht.
+        // Registriere Handler für "localhost:8080/"
         app.get(
                 "/",
                 new Handler() {
                     // Context: https://javalin.io/documentation#context
                     @Override
-                    public void handle(@NotNull Context ctx) throws Exception {
+                    public void handle(@NotNull Context ctx) {
                         ctx.result("Hello World");
                     }
                 });
 
-        // Registriere einen Handler, der aufgerufen wird, wenn eine HTTP-Anfrage auf
-        // "localhost:8080/exit" eingeht.
+        // Registriere Handler für "localhost:8080/exit"
         app.get(
                 "/exit",
                 new Handler() {
                     @Override
-                    public void handle(@NotNull Context ctx) throws Exception {
+                    public void handle(@NotNull Context ctx) {
                         System.exit(0);
                     }
                 });
