7fc568d7f0d236057fb6d66a2553be9160146e60
Carsten Gips <cagix@fh-bielefeld.de>
2022-06-19 16:28:14 +0200

    [VL] Build: Ant (#536)
    * fix yaml
    
    * initial draft
    
    * add Docker for Ant+Ivy/Maven
    
    * kleinere Verbesserungen
    
    * Make: neues Target: runAsRoot
    
    * add snippets
    
    * Make: neues Target: run_as_root
    
    * remove Eclipse/Ant
    
    * add tldr
    
    * schiebe Beispiele zusammen
    
    * gleiche Einstieg mit Gradle ab
    
    * format slides
    
    * format hugo
    
    * add quiz (ant)
    
    * add (newer) quiz (ant)
    
    * add videos

---
+++ markdown/building/src/ant/Hello.java
@@ -0,0 +1,7 @@
+/** Demo für Ant */
+public class Hello {
+    /** Hier gibt's nix zu sehen, gehen Sie weiter :) */
+    public static void main(String[] argv) {
+        System.out.println("Hello World :-)");
+    }
+}
